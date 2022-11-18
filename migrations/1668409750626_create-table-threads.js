bebed3a7962b3b44bb460f783d8126532e01fcc9
Erastus Mauliate <emauliate17@gmail.com>
2022-11-18 09:00:10 +0700

    adding migrate database and eslint

---
+++ migrations/1668409750626_create-table-threads.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNUll: true,
+    },
+    date: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    is_deleted: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: false,
+    },
+  });
+  pgm.addConstraint(
+    'threads',
+    'fk_threads.owner_users.id',
+    'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE',
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('threads');
+};
---
+++ migrations/1668734638670_create-table-replies.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('replies', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    comment_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      notNUll: true,
+    },
+    date: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    content: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    is_deleted: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: false,
+    },
+  });
+  pgm.addConstraint(
+    'replies',
+    'fk_replies.comment_id_comments.id',
+    'FOREIGN KEY(comment_id) REFERENCES comments(id) ON DELETE CASCADE',
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('replies');
+};
